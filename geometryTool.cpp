087ae9e7f6e0d308b48ec7aef24331d1f8a23a6b
Radosveta Shopova <r.shopovaa@gmail.com>
2024-01-20 21:40:26 +0200

    Implements user options output

--- geometryTool.cpp
+++ geometryTool.cpp
@@ -19,22 +19,22 @@ using namespace std;
 constexpr size_t MAX_SIZE = 1024;
 const double EPSILON = 0.0001;
 
-bool Is_Letter(char ch)
+bool checkIsLetter(char ch)
 {
     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
 }
 
-bool Is_Number(char ch)
+bool checkIsNumber(char ch)
 {
     return ch >= '0' && ch <= '9';
 }
 
-double Abs_Value(double num)
+double calculateAbsolute(double num)
 {
     return (num > 0) ? num : -num;
 }
 
-bool Are_Equal(double num1, double num2)
+bool checkAreEqual(double num1, double num2)
 {
     double difference = num1 - num2;
     if (difference < 0)
@@ -57,7 +57,12 @@ bool Is_Valid_Name(const char* symbol)
 
     while (*symbol)
     {
-        if (!Is_Letter(*symbol) || !Is_Number(*symbol) || *symbol == '_')
+        bool isSymbolValid = false;
+        if (checkIsLetter(*symbol) || checkIsNumber(*symbol) || *symbol == '_')
+        {
+            isSymbolValid = true;
+        }
+        if (!isSymbolValid)
         {
             return false;
         }
@@ -80,11 +85,13 @@ void Get_Name_Input(char* name)
 
 }
 
-void Enter_A_Number(double& num)
+void readValidNumber(double& num)
 {
     while (true)
     {
         cin >> num;
+
+        // Check if the input operation succeeded
         if (cin.good())
         {
             break;
@@ -92,26 +99,31 @@ void Enter_A_Number(double& num)
         else
         {
             cout << "Invalid number! Please enter a new number!" << endl;
+
+            // Clear the error flags on cin to enable further input operations
             cin.clear();
+
+            // Ignore the remaining characters in the input buffer up to the newline character
+            // This is to prevent invalid characters from affecting the next input operation
             cin.ignore(100000, '\n');
         }
     }
 }
 
-void Defines_A_Line(char* line, double& a, double& b, double& c)
+void defineLine(char* line, double& a, double& b, double& c)
 {
-    cout << "Enter an appropriate line name: ";
+    cout << "Enter line name: ";
     Get_Name_Input(line);
-    cout << "Enter values for the coefficients of A, B and C on the line:";
+    cout << "Enter the coefficients:" << endl;
 
-    cout << "A = ";
-    Enter_A_Number(a);
+    cout << "Coefficient A: ";
+    readValidNumber(a);
 
-    cout << "B = ";
-    Enter_A_Number(b);
+    cout << "Coefficient B: ";
+    readValidNumber(b);
 
-    cout << "C = ";
-    Enter_A_Number(c);
+    cout << "Coefficient C: ";
+    readValidNumber(c);
 
 }
 
@@ -119,66 +131,74 @@ void Defines_A_Point(char* point, double& x, double& y)
 {
     cout << "Enter an appropriate point name: ";
     Get_Name_Input(point);
-    cout << "Enter values for the coordinates (x, y):";
+    cout << "Enter values for the coordinates (x, y):" << endl;
 
     cout << "x = ";
-    Enter_A_Number(x);
+    readValidNumber(x);
 
     cout << "y = ";
-    Enter_A_Number(y);
+    readValidNumber(y);
 }
 
-void Get_Valid_Coefficient(char* line, double& a, double& b, double& c)
+/**
+ * Validates that at least one of the coefficients A or B is non-zero for a line.
+ * Prompts for re-entry of coefficients if both A and B are zero.
+ *
+ * @param line Pointer to the line's name.
+ * @param a Reference to the coefficient A.
+ * @param b Reference to the coefficient B.
+ * @param c Reference to the coefficient C.
+ */
+void getValidCoefficients(char* line, double& a, double& b, double& c)
 {
-    while (Are_Equal(a, 0) && Are_Equal(b, 0))
+    while (checkAreEqual(a, 0) && checkAreEqual(b, 0))
     {
-        std::cout << "At least one of the coefficients A and B must be non-zero. Please try again."; // Invalid input!
-        Defines_A_Line(line, a, b, c);
+        cout << "Invalid input: Coefficients A and B cannot both be zero. Enter different values." << endl;
+        defineLine(line, a, b, c);
     }
 }
 
 bool Is_Point_On_Line(double x, double y, double a, double b, double c)
 {
     double equation = a * x + b * y + c;
-    return Are_Equal(equation, 0);
+    return checkAreEqual(equation, 0);
 }
 
 void Print_Line_Equation(double a, double b, double c)
 {
-    if (!Are_Equal(a, 0))
+    if (!checkAreEqual(a, 0))
     {
-        std::cout << a << "x";
+        cout << a << "x";
     }
 
-    if (!Are_Equal(b, 0) && !Are_Equal(a, 0))
+    if (!checkAreEqual(b, 0) && !checkAreEqual(a, 0))
     {
-        std::cout << (b > 0 ? " + " : " - ");
+        cout << (b > 0 ? " + " : " - ");
     }
 
-    if (!Are_Equal(b, 0))
+    if (!checkAreEqual(b, 0))
     {
-        if (!Are_Equal(Abs_Value(b), 1) || Are_Equal(a, 0))
+        if (!checkAreEqual(calculateAbsolute(b), 1) || checkAreEqual(a, 0))
         {
-            std::cout << Abs_Value(b);
+            cout << calculateAbsolute(b);
         }
-        std::cout << "y";
+        cout << "y";
     }
 
-    if ((!Are_Equal(a, 0) || !Are_Equal(b, 0)) && !Are_Equal(c, 0))
+    if ((!checkAreEqual(a, 0) || !checkAreEqual(b, 0)) && !checkAreEqual(c, 0))
     {
-        std::cout << (c > 0 ? " + " : " - ");
+        cout << (c > 0 ? " + " : " - ");
     }
 
-    if (!Are_Equal(c, 0))
+    if (!checkAreEqual(c, 0))
     {
-        std::cout << Abs_Value(c);
-        ;
+        cout << calculateAbsolute(c);
     }
 
     // Print "= 0"
-    if (!Are_Equal(a, 0) || !Are_Equal(b, 0) || !Are_Equal(c, 0))
+    if (!checkAreEqual(a, 0) || !checkAreEqual(b, 0) || !checkAreEqual(c, 0))
     {
-        std::cout << " = 0";
+        cout << " = 0";
     }
 }
 
@@ -201,25 +221,25 @@ void Equation_Of_Perpendicular_Line_Passsing_Throuh_A_Point(double x, double y, 
 
 bool Are_Overlapping_Lines(double a1, double b1, double c1, double a2, double b2, double c2)
 {
-    return Are_Equal(a1, a2) && Are_Equal(b1, b2) && Are_Equal(c1, c2);
+    return checkAreEqual(a1, a2) && checkAreEqual(b1, b2) && checkAreEqual(c1, c2);
 }
 
 bool Are_Parallel_Lines(double a1, double b1, double c1, double a2, double b2, double c2)
 {
-    if (Are_Equal(a1, 0) && Are_Equal(a2, 0) || Are_Equal(b1, 0) && Are_Equal(b2, 0))
+    if (checkAreEqual(a1, 0) && checkAreEqual(a2, 0) || checkAreEqual(b1, 0) && checkAreEqual(b2, 0))
     {
         return true;
     }
 
     double coefficientA = a1 / a2;
     double coefficientB = b1 / b2;
-    if (Are_Equal(coefficientA, coefficientB))
+    if (checkAreEqual(coefficientA, coefficientB))
     {
         a2 *= coefficientB;
         b2 *= coefficientB;
         c2 *= coefficientB;
     }
-    if (Are_Equal(a1, a2) && Are_Equal(b1, b2) && !Are_Equal(c1, c2))
+    if (checkAreEqual(a1, a2) && checkAreEqual(b1, b2) && !checkAreEqual(c1, c2))
     {
         return true;
     }
@@ -235,7 +255,7 @@ bool Are_Penperdicular(double a1, double b1, double c1, double a2, double b2, do
 
     double coefficientA = a1 * a2;
     double coefficientB = -b1 * b2;
-    if (Are_Equal(coefficientA, coefficientB))
+    if (checkAreEqual(coefficientA, coefficientB))
     {
         return true;
     }
@@ -257,12 +277,43 @@ void Interception_Point_Of_Two_Lines(double a1, double b1, double c1, double a2,
     Print_Point_Of_Two_Lines(x1, x2, y1, y2);
 }
 
+void showOptions() {
+    cout << "Available Options in Geometry Tool:" << endl;
+    cout << "1. Verify if a specific point is on a defined line." << endl;
+    cout << "2. Output a line parallel to a given line through a specific point." << endl;
+    cout << "3. Output a line perpendicular to a given line at a specific point." << endl;
+    cout << "4. Find intersection point of two lines." << endl;
+}
+
+//void Initial_Choice(double& N) {
+//    cout << endl << "Enter the number of one of the options above: ";
+//    readValidNumber(N);
+//    double allOptions = 12;
+//    if (N < 1 || N > allOptions) {
+//        cout << "Invalid number! Enter a new number!" << endl;
+//        Initial_Choice(N);
+//    }
+//}
+
+void loadUserSelection(double& selection) {
+    const unsigned int MaxOptions = 12;
+    cout << "\nChoose an option (1-" << MaxOptions << "): ";
+
+    readValidNumber(selection);
+
+    bool isInvalidSelection = selection < 1 || selection > MaxOptions;
+    if (isInvalidSelection) {
+        cout << "Choice out of range. Please reselect:" << endl;
+        loadUserSelection(selection);
+    }
+}
+
 void Choise_1_Point_On_Line()
 {
     double a, b, c;
     char lineName[MAX_SIZE];
-    Defines_A_Line(lineName, a, b, c);
-    Get_Valid_Coefficient(lineName, a, b, c);
+    defineLine(lineName, a, b, c);
+    getValidCoefficients(lineName, a, b, c);
 
     double x, y;
     char pointName[MAX_SIZE];
@@ -270,19 +321,22 @@ void Choise_1_Point_On_Line()
 
     if (Is_Point_On_Line(x, y, a, b, c))
     {
-        std::cout << "The point " << pointName << " lies on line " << lineName << std::endl;
+        cout << "The point " << pointName << " lies on line " << lineName << endl;
     }
     else {
-        std::cout << "The point " << pointName << " does not lie on line " << lineName << std::endl;
+        cout << "The point " << pointName << " does not lie on line " << lineName << endl;
     }
 }
 
-void Choise_2_Equation_Of_Line_Parallel_to_Selected_Line_And_Passing_Through_Selected_Point()
+/**
+ * Calculates the equation of a line parallel to a given line and passing through a specified point.
+ */
+void getParallelLineEquation()
 {
     double a, b, c;
     char lineName[MAX_SIZE];
-    Defines_A_Line(lineName, a, b, c);
-    Get_Valid_Coefficient(lineName, a, b, c);
+    defineLine(lineName, a, b, c);
+    getValidCoefficients(lineName, a, b, c);
 
     double x, y;
     char pointName[MAX_SIZE];
@@ -296,16 +350,16 @@ void Choise_3_Equation_Of_Line_Perpendicular_To_Selected_Line_Passsing_Throuh_A_
 {
     double a, b, c;
     char lineName[MAX_SIZE];
-    Defines_A_Line(lineName, a, b, c);
-    Get_Valid_Coefficient(lineName, a, b, c);
+    defineLine(lineName, a, b, c);
+    getValidCoefficients(lineName, a, b, c);
 
     double x, y;
     char pointName[MAX_SIZE];
     Defines_A_Point(pointName, x, y);
     while (!Is_Point_On_Line(x, y, a, b, c))
     {
-        std::cout << "The point does not lie on line. Plese try again.";
-        Defines_A_Line(lineName, a, b, c);
+        cout << "The point does not lie on line. Plese try again.";
+        defineLine(lineName, a, b, c);
     }
     cout << "The equation of the line is: ";
     Equation_Of_Perpendicular_Line_Passsing_Throuh_A_Point(x, y, a, b, c);
@@ -316,25 +370,25 @@ void Choise_4_Find_The_Interception_Point_Of_Two_Lines()
 {
     double a1, b1, c1;
     char lineName1[MAX_SIZE];
-    Defines_A_Line(lineName1, a1, b1, c1);
-    Get_Valid_Coefficient(lineName1, a1, b1, c1);
+    defineLine(lineName1, a1, b1, c1);
+    getValidCoefficients(lineName1, a1, b1, c1);
 
     double a2, b2, c2;
     char lineName2[MAX_SIZE];
-    Defines_A_Line(lineName2, a2, b2, c2);
-    Get_Valid_Coefficient(lineName2, a2, b2, c2);
+    defineLine(lineName2, a2, b2, c2);
+    getValidCoefficients(lineName2, a2, b2, c2);
 
     if (Are_Overlapping_Lines(a1, b1, c1, a2, b2, c2))
     {
         cout << "The two lines are overlapping! Please try again! " << endl;
-        Defines_A_Line(lineName1, a1, b1, c1);
-        Defines_A_Line(lineName2, a2, b2, c2);
+        defineLine(lineName1, a1, b1, c1);
+        defineLine(lineName2, a2, b2, c2);
     }
     if (Are_Parallel_Lines(a1, b1, c1, a2, b2, c2))
     {
         cout << "The two lines are parallel! Please try again! " << endl;
-        Defines_A_Line(lineName1, a1, b1, c1);
-        Defines_A_Line(lineName2, a2, b2, c2);
+        defineLine(lineName1, a1, b1, c1);
+        defineLine(lineName2, a2, b2, c2);
     }
 
     cout << "The intersection point of " << lineName1 << " and " << lineName2 << " is: ";
@@ -342,7 +396,35 @@ void Choise_4_Find_The_Interception_Point_Of_Two_Lines()
     cout << endl;
 }
 
+void options(int N, bool& want_to_quit) {
+
+    switch (N) {
+    case 1: Choise_1_Point_On_Line();break;
+    case 2: getParallelLineEquation();break;
+    case 3: Choise_3_Equation_Of_Line_Perpendicular_To_Selected_Line_Passsing_Throuh_A_Point();break;
+    case 4: Choise_4_Find_The_Interception_Point_Of_Two_Lines();break;
+    case 5:;break;
+    case 6:;break;
+    case 7:;break;
+    case 8:;break;
+    default:
+        cout << "Incorrect input! Please try again and enter a number between 1 and 12.\n";
+        break;
+    }
+}
+
+void start_Program() {
+    bool want_to_quit = true;
+    showOptions();
+
+    while (want_to_quit != false) {
+        double N;
+        loadUserSelection(N);
+        options(N, want_to_quit);
+    }
+}
+
 int main()
 {
-
+    start_Program();
 }
